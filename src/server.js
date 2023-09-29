288511a94939ba7725cb687b7f6d818327c49d81
TheSang88 <phamthesang88@gmail.com>
2023-09-29 12:36:38 +0700

    updatev

--- src/server.js
+++ src/server.js
@@ -2,19 +2,16 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-
+import 'dotenv/config'
 let UserRouter = require('./router/webrouter')
-
-const config = require('./DB.js');
-require('dotenv').config();
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
- 
-mongoose.Promise = global.Promise;
-mongoose.connect(config.DB, { useNewUrlParser: true })
+
+
+mongoose.connect(process.env.MONGODB)
     .then(
         () => { console.log('Database is connected') },
         err => { console.log('Can not connect to the database' + err) }
@@ -24,5 +21,5 @@ app.use('/User', UserRouter);
 
 let PORT = process.env.PORT;
 app.listen(PORT, () => {
-    console.log("ok port:" + PORT);
+    console.log("ok dang chay port:" + PORT);
 });
